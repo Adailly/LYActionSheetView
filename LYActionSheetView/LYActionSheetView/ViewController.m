d420d2c65038df88c71bf29d86743372b81696c0
yang <yang@yangdemacbook-air.local>
2016-10-08 14:31:31 +0800

    1、添加效果图

--- LYActionSheetView/LYActionSheetView/ViewController.m
+++ LYActionSheetView/LYActionSheetView/ViewController.m
@@ -25,18 +25,22 @@
 - (IBAction)ShowActionSheetView:(UIButton *)sender
 {
     // 创建view
-//    LYActionSheetView *sheetView = [[LYActionSheetView alloc] initWithTitle:nil delegate:self actionSheetArray:@[@"小视频", @"拍照", @"从相册中选择"]];
-    
-    // 创建view
-    LYActionSheetView *sheetView = [[LYActionSheetView alloc] initWithTitle:@"是否取消操作" delegate:self actionSheetArray:@[@"确认"]];
-    
-    // 设置选项的颜色
-    sheetView.actionSheetColor = [UIColor redColor];
+    LYActionSheetView *sheetView = [[LYActionSheetView alloc] initWithTitle:nil delegate:self actionSheetArray:@[@"小视频", @"拍照", @"从相册中选择"]];
     
     // 显示view
     [sheetView show];
+
+//    // 创建view
+//    LYActionSheetView *sheetView = [[LYActionSheetView alloc] initWithTitle:@"是否取消操作" delegate:self actionSheetArray:@[@"确认"]];
+//    
+//    // 设置选项的颜色
+//    sheetView.actionSheetColor = [UIColor redColor];
+//    
+//    // 显示view
+//    [sheetView show];
 }
 
+// 代理方法
 - (void)actionSheetView:(LYActionSheetView *)actionSheetView selectedString:(NSString *)selectedString index:(NSInteger)index
 {
     NSLog(@"%@ -- %zd", selectedString, index);
